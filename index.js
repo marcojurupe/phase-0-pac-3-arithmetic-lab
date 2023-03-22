c9639d9050d6ab79bd982a4958f9a490a6453e59
marcojurupe <marco.jurupe@gmail.com>
2023-03-21 22:48:46 -0500

    new index.js

--- index.js
+++ index.js
@@ -1,72 +1,34 @@
-var a, b
+var number,a,b;
 
-function beforeEach() {
-  a = Math.floor(Math.random() * 1000);
-  b = Math.floor(Math.random() * 1000);
+function add(a, b){ 
+  number = a + b;
+  return number;
 }
-
-function add(a,b) {
-    beforeEach();
-    return a+b;
+function subtract(a, b){ 
+  number = a - b;
+  return number;
 }
-
-function subtract(a,b) {
-    beforeEach();
-    return a-b;
+function multiply(a, b){ 
+  number = a * b;
+  return number;
 }
-function multiply(a,b) {
-    beforeEach();
-    return a*b;
+function divide(a, b){
+  number = a / b;
+  return number;
 }
-
-function divide(a,b) {
-    beforeEach();
-    return a/b;
+function increment(a){
+  number = a + 1;
+  return number;
 }
-
-function increment(a) {
-    a++;
-    return(a);
+function decrement(a){ 
+  number = a - 1;
+  return number;
 }
-
-function decrement(a) {
-    a--;
-    return(a);
+function makeInt(a){ 
+  number = parseInt(a, 10);
+  return number;
 }
-
-function makeInt(n,base) {
-    return makeInt;
+function preserveDecimal(a){
+  number = parseFloat(a, 10);
+  return number;
 }
-
-function makeInt(n){
-    parseInt(n,10)
-}
-
-function preserveDecimal(n) {
-    return (parseFloat(n));
-}
-
-function makefloat(NaN) {
-    return makefloat;
-}
-
-////////////////////////////////
-// const n = 0 * 2328;
-// function makeInt(n) {
-//     return n;
-// }
-
-
-// function parseInt(makeInt) {
-//     return (parseInt(n,10));
-// }
-
-// const NaN = sldkjflksjf;
-// function makeInt(NaN) {
-//     return makeInt;
-// }
-
-
-
-
-
